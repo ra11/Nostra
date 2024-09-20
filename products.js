891f357cc84d8f4b20f495aee22d49dcfd555794
ra11 <rajeswarivinayagam05@gmail.com>
2024-09-20 21:54:16 +0400

    updated nostra

---
+++ products.js
@@ -0,0 +1,83 @@
+const products = [
+    {
+        id:1,
+        name:"Floral Summer Shirt",
+        src:"f1.jpg",
+        desc:"",
+        price:220,
+        tags:["new","blue","summer"]
+    },
+
+    {
+        id:2,
+        name:"Summer Green",
+        src:"f2.jpg",
+        desc:"",
+        price:260,
+        tags:["new","green","beach"]
+    },
+
+    {
+        id:3,
+        name:"Party Floral Shirt",
+        src:"f3.jpg",
+        desc:"",
+        price:399,
+        tags:["old","red","party"]
+    },
+
+    {
+        id:4,
+        name:"Floral Summer Shirt",
+        src:"f4.jpg",
+        desc:"",
+        price:399,
+        tags:["old","white","beach"]
+    },
+    {
+        id:5,
+        name:"Beach Shirt",
+        src:"f5.jpg",
+        desc:"",
+        price:579,
+        tags:["old","white","beach"]
+    }
+,
+    {
+        id:6,
+        name:"Shirt Party Red",
+        src:"f6.jpg",
+        desc:"",
+        price:579,
+        tags:["old","red","party"]
+    },
+
+    {
+        id:7,
+        name:"Party Floral Shirt",
+        src:"f3.jpg",
+        desc:"",
+        price:399,
+        tags:["old","red","party"]
+    },
+    {
+        id:8,
+        name:"Shirt Party Red",
+        src:"f6.jpg",
+        desc:"",
+        price:579,
+        tags:["old","red","party"]
+    },
+    {
+        id:9,
+        name:"Floral Summer Shirt",
+        src:"f1.jpg",
+        desc:"",
+        price:220,
+        tags:["new","blue","summer"]
+    },
+
+
+]
+
+export {products}
